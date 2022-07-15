488ca697253dd111f8f354ebf3cab7c6e4052d7b
zhangyongqing-sj@bestpay.com.cn <zhangyongqing-sj@bestpay.com.cn>
2022-07-15 12:34:21 +0800

    部署脚本修改

--- script/deploy.sh
+++ script/deploy.sh
@@ -14,9 +14,22 @@ function killPidByServicePort(){
     fi
 }
 
+function getPidByServicePort(){
+  servicePort=$1
+  netstat -nlp > /opt/zyq/tempPid.txt
+    pid=`cat /opt/zyq/tempPid.txt | grep :$servicePort | awk '{print $7}' | awk -F "/" '{ print $1 }'`
+    rm -f /opt/zyq/tempPid.txt
+    return pid
+}
+
 echo "stop JenkinsDemo ......"
 killPidByServicePort $servicePort
 
 echo "start JenkinsDemo ......"
 nohup java -jar $jenkinsWorkSpace/target/jenkins-0.0.1-SNAPSHOT.jar >/data/jenkins-demo.log 2>&1 &
+
+echo "get Pid..."
+pid=getPidByServicePort $servicePort
+echo "pid: $pid"
+
 echo "auto deploy done ..."
